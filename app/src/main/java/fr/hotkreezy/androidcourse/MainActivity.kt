017dc8b6217ac7a7b1cf5cb551d4d2fb208553db
HoTKreeZy <111439147+HoTKreeZy@users.noreply.github.com>
2026-09-07 20:13:43 +0200

    refactor: extract hardcoded strings to resources
    - Add `happy_birthday_text` and `signature_text` to `strings.xml`.
    - Update `GreetingImage` calls in `MainActivity` and `GreetingPreview` to use `stringResource`.
    - Add necessary import for `androidx.compose.ui.res.stringResource`.

--- app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
+++ app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,8 +34,8 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 				) { innerPadding ->
 					GreetingImage(
-						message = "Happy Birthday Sam!",
-						from = "From Emma",
+						message = stringResource(R.string.happy_birthday_text),
+						from = stringResource(R.string.signature_text),
 						modifier = Modifier.padding(innerPadding),
 					)
 				}
@@ -93,6 +94,9 @@ fun GreetingImage(
 @Composable
 fun GreetingPreview() {
 	AndroidCourseTheme {
-		GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+		GreetingImage(
+			message = stringResource(R.string.happy_birthday_text),
+			from = stringResource(R.string.signature_text)
+		)
 	}
 }
