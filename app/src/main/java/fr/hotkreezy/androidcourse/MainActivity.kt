4358836355b4356268c5a122c3c61191a85e15ba
HoTKreeZy <111439147+HoTKreeZy@users.noreply.github.com>
2026-09-07 21:11:10 +0200

    refactor: extract Greeting composables to a separate file

--- app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
+++ app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
@@ -4,24 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import fr.hotkreezy.androidcourse.ui.GreetingImage
 import fr.hotkreezy.androidcourse.ui.theme.AndroidCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,59 +32,3 @@ class MainActivity : ComponentActivity() {
 	}
 }
 
-@Composable
-fun GreetingText(
-	message: String,
-	from: String,
-	modifier: Modifier = Modifier,
-) {
-	Column(
-		modifier = modifier,
-		verticalArrangement = Arrangement.Center,
-	) {
-		Text(
-			text = message,
-			fontSize = 100.sp,
-			textAlign = TextAlign.Center,
-			lineHeight = 116.sp,
-		)
-		Text(
-			text = from,
-			fontSize = 36.sp,
-			modifier = Modifier
-				.align(alignment = Alignment.End)
-				.padding(16.dp),
-		)
-	}
-}
-
-@Composable
-fun GreetingImage(
-	message: String,
-	from: String,
-	modifier: Modifier = Modifier,
-) {
-	val image = painterResource(R.drawable.androidparty)
-	Box(
-		modifier = modifier,
-	) {
-		Image(
-			painter = image,
-			contentDescription = null,
-			contentScale = ContentScale.Crop,
-			alpha = 0.5F,
-		)
-		GreetingText(message, from, Modifier.fillMaxSize())
-	}
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	AndroidCourseTheme {
-		GreetingImage(
-			message = stringResource(R.string.happy_birthday_text),
-			from = stringResource(R.string.signature_text)
-		)
-	}
-}
