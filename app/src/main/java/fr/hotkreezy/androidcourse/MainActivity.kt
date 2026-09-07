0eed817a9baa56892ea96717250da851877cae07
HoTKreeZy <111439147+HoTKreeZy@users.noreply.github.com>
2026-09-07 16:37:35 +0200

    refactor: recreate my own Greeting composable
    - Rename `Greeting` composable to `GreetingText`.
    - Update the display message to "Happy Birthday Sam!" in `MainActivity` and `GreetingPreview`.
    - Change the composable parameter from `name` to `message`.
    - Simplify `Text` element in `GreetingText` by removing the internal modifier application.

--- app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
+++ app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			AndroidCourseTheme {
 				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Greeting(
-						name = "Android",
+					GreetingText(
+						message = "Happy Birthday Sam!",
 						modifier = Modifier.padding(innerPadding)
 					)
 				}
@@ -31,17 +31,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
+fun GreetingText(
+	message: String,
+	modifier: Modifier = Modifier,
+) {
+	Text(text = message)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
 	AndroidCourseTheme {
-		Greeting("Android")
+		GreetingText(message = "Happy Birthday Sam!")
 	}
 }
