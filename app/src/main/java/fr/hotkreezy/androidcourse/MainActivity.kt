976839ba37de6d336bdf1666264dca1b1c87f0ba
HoTKreeZy <111439147+HoTKreeZy@users.noreply.github.com>
2026-09-13 17:27:00 +0200

    feat: implement bill amount input and tip calculation
    - Introduce `EditNumberField` composable using `OutlinedTextField` for user input.
    - Manage input state using `rememberTextFieldState` within `TipCalculatorLayout`.
    - Calculate and display the tip amount dynamically based on the user-entered bill amount.
    - Configure `EditNumberField` with a numeric keyboard and single-line limit.

--- app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
+++ app/src/main/java/fr/hotkreezy/androidcourse/MainActivity.kt
@@ -8,15 +8,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.hotkreezy.androidcourse.ui.theme.AndroidCourseTheme
@@ -42,6 +49,10 @@ class MainActivity : ComponentActivity() {
 fun TipCalculatorLayout(
 	modifier: Modifier = Modifier
 ) {
+	val textState = rememberTextFieldState()
+	val amount = textState.text.toString().toDoubleOrNull() ?: 0.0
+	val tip = calculateTip(amount)
+
 	Column(
 		modifier = modifier
 			.fillMaxSize()
@@ -55,12 +66,32 @@ fun TipCalculatorLayout(
 				.padding(bottom = 16.dp, top = 40.dp)
 				.align(alignment = Alignment.Start)
 		)
+		EditNumberField(
+			state = textState,
+			modifier = Modifier
+				.padding(bottom = 32.dp)
+				.fillMaxWidth()
+		)
 		Text(
-			text = "Tip Amount: $0.00",
+			text = "Tip Amount: $tip",
 			style = MaterialTheme.typography.displaySmall
 		)
 		Spacer(modifier = Modifier.height(150.dp))
 	}
+}
+
+@Composable
+fun EditNumberField(
+	state: TextFieldState,
+	modifier: Modifier = Modifier,
+) {
+	OutlinedTextField(
+		state = state,
+		modifier = modifier,
+		label = { Text("Bill Amount") },
+		keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+		lineLimits = TextFieldLineLimits.SingleLine,
+	)
 }
 
 @Preview(showBackground = true)
